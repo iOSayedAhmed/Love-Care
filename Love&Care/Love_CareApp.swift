44d8c8ba547e947519126c3359d8133e269cd2b4
iOSayedAhmed <elsayed1ahmed0@gmail.com>
2022-06-13 11:15:51 +0200

    Initial Commit

---
+++ Love&Care/Love_CareApp.swift
@@ -0,0 +1,17 @@
+//
+//  Love_CareApp.swift
+//  Love&Care
+//
+//  Created by Develop on 13/06/2022.
+//
+
+import SwiftUI
+
+@main
+struct Love_CareApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
